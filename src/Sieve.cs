7fdd70706a2d30963b8df4904243a7d78c4b22bf
Victor Nova <lostfreeman@gmail.com>
2024-04-16 22:46:01 -0700

    Sieve can be saved and loaded

--- src/Sieve.cs
+++ src/Sieve.cs
@@ -1,14 +1,18 @@
 ﻿namespace Caching;
 
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
 /// <summary>
 /// Implements a SIEVE cache eviction policy as described in
 /// <see cref="https://cachemon.github.io/SIEVE-website/blog/2023/12/17/sieve-is-simpler-than-lru/">SIEVE</see>
 /// </summary>
 /// <typeparam name="TKey">Type of the cache key</typeparam>
 public class Sieve<TKey> {
-    readonly LinkedList<Entry> expiration = new();
-    readonly Dictionary<TKey, LinkedListNode<Entry>> entries;
-    LinkedListNode<Entry>? hand;
+    internal readonly LinkedList<Entry> expiration = new();
+    internal readonly Dictionary<TKey, LinkedListNode<Entry>> entries;
+    internal LinkedListNode<Entry>? hand;
 
     public Sieve(int capacity, IEqualityComparer<TKey>? comparer = null) {
         if (capacity <= 1)
@@ -58,7 +62,93 @@ public class Sieve<TKey> {
     public int Count => this.entries.Count;
     public int Capacity { get; }
 
-    struct Entry {
+    public void Save(Stream stream, Action<TKey, Stream> keyWriter, CancellationToken cancel = default) {
+        if (stream is null) throw new ArgumentNullException(nameof(stream));
+        if (keyWriter is null) throw new ArgumentNullException(nameof(keyWriter));
+
+        stream.WriteByte(0); // Version
+
+        WriteI32(stream, this.Capacity);
+        WriteI32(stream, this.expiration.Count);
+
+        int handIndex = -1;
+        int index = 0;
+        foreach (var node in Enumerate(this.expiration)) {
+            cancel.ThrowIfCancellationRequested();
+
+            stream.WriteByte(node.Value.visited ? (byte)1 : (byte)0);
+            keyWriter(node.Value.key, stream);
+
+            if (handIndex < 0 && node == this.hand)
+                handIndex = index;
+            index++;
+        }
+
+        if (handIndex >= 0)
+            WriteI32(stream, handIndex);
+    }
+
+    internal static void WriteI32(Stream stream, int value) {
+        if (value < 0) throw new ArgumentOutOfRangeException(nameof(value), "Value must be non-negative.");
+
+        stream.WriteByte((byte)value);
+        stream.WriteByte((byte)(value >> 8));
+        stream.WriteByte((byte)(value >> 16));
+        stream.WriteByte((byte)(value >> 24));
+    }
+
+    public static Sieve<TKey> Load(Stream stream,
+                                   Func<Stream, TKey> keyReader,
+                                   IEqualityComparer<TKey>? comparer = null) {
+        if (stream is null) throw new ArgumentNullException(nameof(stream));
+        if (keyReader is null) throw new ArgumentNullException(nameof(keyReader));
+
+        int version = stream.ReadByte();
+        if (version != 0) throw new InvalidDataException("Invalid version");
+
+        int capacity = ReadI32(stream);
+        int count = ReadI32(stream);
+        var sieve = new Sieve<TKey>(capacity, comparer);
+        for (int i = 0; i < count; i++) {
+            bool visited = ReadByte(stream) != 0;
+            TKey key = keyReader(stream);
+            var entry = new Entry { visited = visited, key = key };
+            sieve.entries[key] = sieve.expiration.AddLast(entry);
+        }
+
+        if (count > 0) {
+            int handIndex = Sieve<TKey>.ReadI32(stream);
+            sieve.hand = Sieve<TKey>.Enumerate(sieve.expiration).ElementAt(handIndex);
+        }
+
+        return sieve;
+    }
+
+    internal int HandIndex => Enumerate(this.expiration).TakeWhile(node => node != this.hand).Count();
+
+    static IEnumerable<LinkedListNode<Entry>> Enumerate(LinkedList<Entry> list) {
+        var node = list.First;
+        while (node is not null) {
+            yield return node;
+            node = node.Next;
+        }
+    }
+
+    static byte ReadByte(Stream stream) {
+        int b = stream.ReadByte();
+        if (b < 0) throw new EndOfStreamException();
+        return (byte)b;
+    }
+
+    internal static int ReadI32(Stream stream) {
+        int result = ReadByte(stream);
+        result |= ReadByte(stream) << 8;
+        result |= ReadByte(stream) << 16;
+        result |= ReadByte(stream) << 24;
+        return result;
+    }
+
+    internal struct Entry {
         internal bool visited;
         internal TKey key;
     }
--- test/SieveTests.cs
+++ test/SieveTests.cs
@@ -1,5 +1,8 @@
 namespace Caching;
 
+using System.IO;
+using System.Threading.Tasks;
+
 public class SieveTests {
     [Fact]
     public void DoesNotOvergrow() {
@@ -12,5 +15,31 @@ public class SieveTests {
             Assert.Equal(expected, sieve.Count);
             Assert.Equal(expected, added - evicted);
         }
+    }
+
+    [Fact]
+    public void SaveRoundtrip() {
+        var sieve = new Sieve<int>(capacity: 10);
+
+        for (int added = 1; added <= 20; added++) {
+            sieve.Access(added, out _);
+        }
+        for(int accessed = 20; accessed >= 18; accessed--) {
+            sieve.Access(accessed, out _);
+        }
+
+        using var stream = new MemoryStream();
+        sieve.Save(stream, (key, s) => Sieve<int>.WriteI32(s, key));
+
+        stream.Position = 0;
+
+        var loaded = Sieve<int>.Load(stream, s => Sieve<int>.ReadI32(s));
+
+        Assert.Equal(sieve.Count, loaded.Count);
+        Assert.Equal(sieve.Capacity, loaded.Capacity);
+        Assert.Equal(sieve.HandIndex, loaded.HandIndex);
+        Assert.Equal(sieve.expiration, loaded.expiration);
+        Assert.Equal(sieve.entries.OrderBy(kv => kv.Key).Select(kv => (kv.Key, kv.Value.Value)),
+            loaded.entries.OrderBy(kv => kv.Key).Select(kv => (kv.Key, kv.Value.Value)));
     }
 }
